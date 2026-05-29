59b08dcf2446211e89db25fd7a975d104ea3c367
Maciej Mazurek <mazurekk67@gmail.com>
2026-05-29 16:07:07 +0200

    Minor fix related to comments

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,17 +6,15 @@ app = FastAPI(title="qr-safety-checker-backend", version="1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # W produkcji zmień na konkretny adres URL
+    allow_origins=["*"],  # To be changed to specific origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Definiujemy, jakich danych oczekujemy od frontendu (bezpieczne sprawdzanie typów)
 class UrlCheckRequest(BaseModel):
     url: HttpUrl
 
-# Główny endpoint, do którego telefon będzie wysyłał odczytany link
 @app.post("/api/check-url")
 def check_url(payload: UrlCheckRequest):
     scanned_url = str(payload.url)
